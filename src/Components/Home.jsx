7ad00db72850b9ab455322f1724e5c3c9da1acef
Hypekage <jmestmnag@gmail.com>
2022-12-01 20:00:05 +0545

    structure created now implementing css in home page

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,29 +1,53 @@
-import React from 'react';
-import {NavLink} from 'react-router-dom'
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Home = () => {
-
-    const arr = [
-        {
-            id:'1'
-        }
-    ]
+  const arr = [
+    {
+      id: "1",
+      title: "30 Min Meeting",
+      time: "30m",
+    },
+    {
+      id: "2",
+      title: "15 Min Meeting",
+      time: "15m",
+    },
+  ];
 
   return (
     <>
-    <div className="container-fluid">
+      <div className="container-fluid">
         <div className="container">
-            <div className="topic">
-                <h3>Saroj Ghalan</h3>
-                <p>test</p>
-            </div>
-            <div className="meeting-main">
-
-            </div>
+          <div className="topic">
+            <h3>Saroj Ghalan</h3>
+            <p>test</p>
+          </div>
+          <div className="meeting-main">
+            {arr.map((get, keys) => {
+              return (
+                <>
+                  <div className="meeting-card" key={keys}>
+                    <h4>{get.title}</h4>
+                    <div className="time">
+                        <ul>
+                            <li>
+                                <p>{get.time}</p>
+                            </li>
+                            <li>
+                                <p>1-on-1</p>
+                            </li>
+                        </ul>
+                    </div>
+                  </div>
+                </>
+              );
+            })}
+          </div>
         </div>
-    </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
