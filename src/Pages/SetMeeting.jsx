66cfdcf70a0a51a997561179f4e45a817bd7e30b
Hypekage <jmestmnag@gmail.com>
2022-12-02 09:28:34 +0545

    skeleton created

--- src/Pages/SetMeeting.jsx
+++ src/Pages/SetMeeting.jsx
@@ -1,11 +1,9 @@
 import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
-import Calendar from "react-calendar";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
 const SetMeeting = () => {
-  const [value, onChange] = useState(new Date());
   const [startDate, setStartDate] = useState(new Date());
 
   const halhday = [
@@ -49,102 +47,104 @@ const SetMeeting = () => {
       <div className="home">
         <div className="container-fluid">
           <div className="container">
-            <div className="row">
-              <div className="col-md-4">
-                <div className="left-meet">
-                  <div className="left-meeting-head">
-                    <img src="" alt="" />
-                    <h4>Saroj Ghalan</h4>
-                    <h2>30 Min Meeting</h2>
+            <div className="meeting-schedule-main">
+              <div className="row">
+                <div className="col-md-4">
+                  <div className="left-meet">
+                    <div className="left-meeting-head">
+                      <img src="" alt="" />
+                      <h4>Saroj Ghalan</h4>
+                      <h2>30 Min Meeting</h2>
+                    </div>
+                    <div className="left-meet-content">
+                      <ul>
+                        <li>
+                          <p>
+                            <span></span>Call video
+                          </p>
+                        </li>
+                        <li>
+                          <p>
+                            <span></span>30 Minutes
+                          </p>
+                        </li>
+                        <li>
+                          <p>
+                            <span></span>Asia/Kathmandu
+                          </p>
+                        </li>
+                      </ul>
+                    </div>
                   </div>
-                  <div className="left-meet-content">
+                </div>
+                <div className="col-md-5">
+                  <div className="right-meet">
+                    {/* <Calendar onChange={onChange} value={value} /> */}
+                    <DatePicker
+                      selected={startDate}
+                      onChange={(date: Date) => setStartDate(date)}
+                      id="click"
+                    />
+                  </div>
+                </div>
+                <div className="col-md-3">
+                  <div className="time-picker">
                     <ul>
                       <li>
-                        <p>
-                          <span></span>Call video
-                        </p>
+                        <p>Sun,August 4</p>
                       </li>
                       <li>
-                        <p>
-                          <span></span>30 Minutes
-                        </p>
-                      </li>
-                      <li>
-                        <p>
-                          <span></span>Asia/Kathmandu
-                        </p>
+                        <ul class="nav nav-tabs" id="myTab" role="tablist">
+                          <li class="nav-item" role="presentation">
+                            <button
+                              class="nav-link active"
+                              id="home-tab"
+                              data-bs-toggle="tab"
+                              data-bs-target="#home"
+                              type="button"
+                              role="tab"
+                              aria-controls="home"
+                              aria-selected="true"
+                            >
+                              12h
+                            </button>
+                          </li>
+                          <li class="nav-item" role="presentation">
+                            <button
+                              class="nav-link"
+                              id="profile-tab"
+                              data-bs-toggle="tab"
+                              data-bs-target="#profile"
+                              type="button"
+                              role="tab"
+                              aria-controls="profile"
+                              aria-selected="false"
+                            >
+                              24h
+                            </button>
+                          </li>
+                        </ul>
+                        <div class="tab-content" id="myTabContent">
+                          <div
+                            class="tab-pane fade show active"
+                            id="home"
+                            role="tabpanel"
+                            aria-labelledby="home-tab"
+                          >
+                            jaja
+                          </div>
+                          <div
+                            class="tab-pane fade"
+                            id="profile"
+                            role="tabpanel"
+                            aria-labelledby="profile-tab"
+                          >
+                            kaka
+                          </div>
+                        </div>
                       </li>
                     </ul>
                   </div>
-                </div>
-              </div>
-              <div className="col-md-5">
-                <div className="right-meet">
-                  {/* <Calendar onChange={onChange} value={value} /> */}
-                  <DatePicker
-                    selected={startDate}
-                    onChange={(date: Date) => setStartDate(date)}
-                    id="click"
-                  />
-                </div>
-              </div>
-              <div className="col-md-3">
-                <div className="time-picker">
-                  <ul>
-                    <li>
-                      <p>Sun,August 4</p>
-                    </li>
-                    <li>
-                      <ul class="nav nav-tabs" id="myTab" role="tablist">
-                        <li class="nav-item" role="presentation">
-                          <button
-                            class="nav-link active"
-                            id="home-tab"
-                            data-bs-toggle="tab"
-                            data-bs-target="#12h"
-                            type="button"
-                            role="tab"
-                            aria-controls="12h"
-                            aria-selected="true"
-                          >
-                            12h
-                          </button>
-                        </li>
-                        <li class="nav-item" role="presentation">
-                          <button
-                            class="nav-link"
-                            id="profile-tab"
-                            data-bs-toggle="tab"
-                            data-bs-target="#24h"
-                            type="button"
-                            role="tab"
-                            aria-controls="24h"
-                            aria-selected="false"
-                          >
-                            24h
-                          </button>
-                        </li>
-                      </ul>
-                      <div class="tab-content" id="myTabContent">
-                        <div
-                          class="tab-pane fade show active"
-                          id="12h"
-                          role="tabpanel"
-                          aria-labelledby="home-tab"
-                        >
-                          first one
-                        </div>
-                        <div
-                          class="tab-pane fade"
-                          id="24h"
-                          role="tabpanel"
-                          aria-labelledby="profile-tab"
-                        >
-                          second one
-                        </div>
-                      </div>
-                    </li>
-                  </ul>
                 </div>
               </div>
             </div>
